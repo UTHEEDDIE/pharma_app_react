3f7f16d4484a56445c87d512e81310832c7e5c12
Chistyakov Eduard <53441703+UTHEEDDIE@users.noreply.github.com>
2023-08-26 18:15:38 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useEffect } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Button/Header/Header';
+import Header from './components/Header/Header';
+import { Route, Routes} from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 
 function App() {
@@ -14,8 +17,11 @@ function App() {
  
   return (
     <div className="App">
-      <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Header /> 
+      <Routes>
+        <Route index element={<ProductList />}/>
+        <Route path={'/form'} element={<Form />}/>
+      </Routes>     
     </div>
   );
 }
