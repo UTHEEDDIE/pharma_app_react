8e5d47be7b29caa00003be749eccdc789d6425de
Chistyakov Eduard <53441703+UTHEEDDIE@users.noreply.github.com>
2023-08-26 17:52:24 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
-const tg = window.Telegram.WebApp;
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
   
   useEffect(() => {
      tg.ready();
   }, [])
 
  
-
   return (
     <div className="App">
-    work
-     <button onClick={onClose}>Закрыть</button>
+     <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
