093b5f175578d46236decbf8530abc884d042caa
Chistyakov Eduard <53441703+UTHEEDDIE@users.noreply.github.com>
2023-08-28 17:14:34 +0300

    second commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Form.css'
+import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
     const [country, setCountry] = useState('');
@@ -20,25 +21,25 @@ const Form = () => {
 
 
     return (
-        <div className={'form'}>
+        <div className={"form"}>
             <h3>Введите ваши данные</h3>
-            <input 
-               className={'input'} 
-               type="text" 
-               placeholder={'Страна'}
-               value={country}
-               onChange={onChangeCountry}
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
             />
-            <input 
-               className={'input'} 
-               type="text" 
-               placeholder={'Улица'}
-               value={street}
-               onChange={onChangeStreet}
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
             />
             <select value={subject} onChange={onChangeSubject} className={'select'}>
-               <option value={'physical'}>Физ.лицо</option>
-               <option value={'legal'}>Юр.лицо</option>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
             </select>
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,17 @@
-import React from "react";
+import React from 'react';
 import Button from "../Button/Button";
-import { useTelegram } from "../../hooks/useTelegram";
-import './Header.css'
+import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
 
-
     return (
         <div className={'header'}>
-          <Button onClick={onClose}>Закрыть</Button> 
-          <span className={'username'}>
-            {user?.username}
-          </span>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
         </div>
     );
 };
